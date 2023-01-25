b98d8b18706e77f52261839b72bccb0cb61417c4
Pere Solà Claver <pere.solaclaver@gmail.com>
2023-01-25 22:14:47 +0000

    Add playstore badge

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
-import { navbar } from "./NavBar.module.css";
+import { navbar, imagecontainer } from "./NavBar.module.css";
+import { StaticImage } from "gatsby-plugin-image";
 
 const NavBar = () => {
   const data = useStaticQuery(graphql`
@@ -30,19 +31,33 @@ const NavBar = () => {
   });
   const arrayWithHomeFirst = [homeNode, ...filteredPages];
   return (
-      <ul className={navbar}>
-        {arrayWithHomeFirst.map((page, index) => {
-          return (
-            <li key={index}>
-              <Link to={page.node.path}>
-                {page.node.path === "/"
-                  ? "HOME"
-                  : page.node.path.split("/")[1].toUpperCase()}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+    <ul className={navbar}>
+      {arrayWithHomeFirst.map((page, index) => {
+        return (
+          <li key={index}>
+            <Link to={page.node.path}>
+              {page.node.path === "/"
+                ? "HOME"
+                : page.node.path.split("/")[1].toUpperCase()}
+            </Link>
+          </li>
+        );
+      })}
+      <li>
+        <div className={imagecontainer}>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.kombucha_making_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <StaticImage
+              alt="Download app from Play store"
+              src="../images/google-play-badge.png"
+            />
+          </a>
+        </div>
+      </li>
+    </ul>
   );
 };
 
